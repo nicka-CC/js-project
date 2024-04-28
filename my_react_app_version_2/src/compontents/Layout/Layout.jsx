e058f0dde9e42f6bfa40115f0409a948acc614bb
nicka-CC <128600081+nicka-CC@users.noreply.github.com>
2024-04-28 22:59:27 +0300

    Reapply "."
    This reverts commit a1b3f38d6dbb087c294bc3852aceff1672b39251.

--- my_react_app_version_2/src/compontents/Layout/Layout.jsx
+++ my_react_app_version_2/src/compontents/Layout/Layout.jsx
@@ -29,8 +29,7 @@ import {
   IoSettings,
 } from "react-icons/io5"; // Или другой подходящий подкаталог, в зависимости от иконки
 
-import Title from "antd/es/typography/Title";
-import Text from "antd/es/typography/Text";
+import RightSidebar from "../RightSidebar/rightSidebar";
 
 export const PageLayout = ({ children }) => {
   const navigate = useNavigate();
@@ -69,42 +68,12 @@ export const PageLayout = ({ children }) => {
     },
     {
       id: 5,
-      label: "Настройки",
+      label: "Профиль",
       key: 5,
-      link: "/auth",
+      link: "/account",
       icon: <IoSettings size={18} />,
     },
   ];
-
-  const { posts } = useSelector((state) => state.postReducer);
-
-  const user = JSON.parse(localStorage.getItem("user"));
-
-  const createRandomUser = () => {
-    return {
-      id: faker.string.uuid(),
-      name: faker.internet.userName(),
-      email: faker.internet.email(),
-      avatar: faker.image.avatar(),
-      number: faker.number.int(),
-    };
-  };
-
-  const [avatar, setAvatarFile] = useState();
-
-  const [uploadAvatar] = useLazyUploadAvatarQuery();
-
-  const sendUploadAvatar = () => {
-    let formData = new FormData();
-
-    formData.append("file", avatar[0]);
-    formData.append("user", JSON.stringify(user));
-    uploadAvatar(formData);
-  };
-
-  const mock_users = faker.helpers.multiple(createRandomUser, {
-    count: 5,
-  });
 
   const footerMenuItems = [
     {
@@ -115,7 +84,12 @@ export const PageLayout = ({ children }) => {
       icon: <IoSettings size={18} />,
     },
   ];
-
+  const handleMenuClick = (e) => {
+    const item = menuItems.find((item) => item.key === e.key);
+    if (item) {
+      navigate(item.link);
+    }
+  };
   return (
     <Layout>
       {/* <Content> */}
@@ -142,6 +116,7 @@ export const PageLayout = ({ children }) => {
             items={menuItems}
             inlineCollapsed={collapseMenu}
             className={classes.sidebarMenu}
+            onClick={handleMenuClick}
           />
           <Menu
             theme="dark"
@@ -153,175 +128,10 @@ export const PageLayout = ({ children }) => {
             className={classes.sidebarMenu}
           />
         </div>
-
         <Content className={classes.content}>
           <Outlet />
         </Content>
-        <div
-          style={{
-            width: "460px",
-            padding: "40px",
-            background: "var(--rightslide-background-color)",
-            margin: "5px 5px 5px 0px",
-            borderRadius: "0px 20px 20px 0px",
-            overflowY: "auto",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              gap: "24px",
-              justifyContent: "end",
-            }}
-          >
-            <div style={{ display: "flex", flexDirection: "column" }}>
-              <Typography.Text
-                level={4}
-                style={{
-                  marginBottom: 0,
-                  lineHeight: 1,
-                  fontWeight: 600,
-                  fontSize: 18,
-                }}
-              >
-                {user.name}
-              </Typography.Text>
-              <Typography.Text>{user.email}</Typography.Text>
-            </div>
-            <div>
-              <label htmlFor="upload_avatar">
-                <Avatar
-                  size={42}
-                  src={`http://localhost:3555/uploads/${user.avatar}`}
-                ></Avatar>
-                <input
-                  name="upload_avatar"
-                  id="upload_avatar"
-                  hidden
-                  type="file"
-                  onChange={(el) => setAvatarFile(el.target.files)}
-                  placeholder={"Upload"}
-                />
-              </label>
-              {avatar && avatar.length > 0 && (
-                <Button onClick={() => sendUploadAvatar()}>Upload</Button>
-              )}
-            </div>
-          </div>
-          <Title
-            style={{ color: "var(--primary-color)", fontWeight: "600" }}
-          >{`${new Date().getUTCDate()}, ${new Date().getUTCFullYear()}`}</Title>
-
-          <Calendar
-            fullscreen={false}
-            mode="month"
-            cellRender={(value) => {
-              let dates = [];
-              for (let obj of posts) {
-                for (let key in obj) {
-                  for (let day in obj[key]) {
-                    if (obj[key][day].length > 0) {
-                      dates.push(day.split(",")[1]);
-                    }
-                  }
-                }
-              }
-
-              if (dates.includes(value.format("DD.MM.YYYY"))) {
-                return <Badge status={"success"} text={""}></Badge>;
-              } else {
-                return <Badge />;
-              }
-            }}
-            headerRender={({ value, type, onChange, onTypeChange }) => {
-              const start = 0;
-              const end = 12;
-              const monthOptions = [];
-
-              let current = value.clone();
-              const localeData = value.localeData();
-              const months = [];
-              for (let i = 0; i < 12; i++) {
-                current = current.month(i);
-                months.push(localeData.monthsShort(current));
-              }
-              const month = value.month();
-
-              for (let i = start; i < end; i++) {
-                monthOptions.push(
-                  <Select.Option key={i} value={i} className="month-item">
-                    {months[i]}
-                  </Select.Option>
-                );
-              }
-
-              return (
-                <div style={{ padding: 8 }}>
-                  <Row gutter={8}>
-                    <Col>
-                      <Select
-                        size="small"
-                        dropdownMatchSelectWidth={false}
-                        value={month}
-                        onChange={(newMonth) => {
-                          const now = value.clone().month(newMonth);
-                          onChange(now);
-                        }}
-                      >
-                        {monthOptions}
-                      </Select>
-                    </Col>
-                  </Row>
-                </div>
-              );
-            }}
-          ></Calendar>
-
-          <div>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <Title
-                level={3}
-                style={{ color: "var(--primary-color)", fontWeight: "600" }}
-              >
-                Online Users:
-              </Title>
-              <Button
-                type="link"
-                level={5}
-                style={{
-                  color: "var(--primary-color)",
-                  fontWeight: "400",
-                  fontSize: "18px",
-                }}
-              >
-                See all:
-              </Button>
-            </div>
-            <div
-              style={{ display: "flex", flexDirection: "column", gap: "16px" }}
-            >
-              {mock_users.map((user, index) => {
-                return (
-                  <div style={{ display: "flex", gap: "6px" }}>
-                    <Avatar size={50} icon={<IoPerson />} src={user.avatar} />
-                    <div style={{ display: "flex", flexDirection: "column" }}>
-                      <Text>{user.name}</Text>
-                      <Text>{user.number}</Text>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </div>
+        <RightSidebar style={{ background: "#fff" }} />
       </Layout>
       {/* </Content> */}
     </Layout>
