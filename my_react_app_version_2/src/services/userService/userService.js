a2eabf06a6d2c0ee162739b7f3258baf5f0af5f4
nicka-CC <128600081+nicka-CC@users.noreply.github.com>
2024-04-28 22:59:45 +0300

    Revert "."
    This reverts commit 0ad3809bdbe3e284260f260f81823df0e576c0a2.

--- my_react_app_version_2/src/services/userService/userService.js
+++ my_react_app_version_2/src/services/userService/userService.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const userService = createApi({
   reducerPath: "userService",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3555" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://127.0.0.1:3555" }),
   endpoints: (builder) => ({
     registerUser: builder.query({
       query: (body) => ({
